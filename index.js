5cd7048ff93da633cdaabecd562c11edb0645ed9
Rahul Agarwal <rahula@ZC-RahulAs-MacBook-Pro.local>
2022-10-12 11:40:27 +0530

    Home page added

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello world");
-})
+});
+
+app.get('/home', (req, res) => {
+    res.send("Hurray!!! you hit the home page");
+});
 
 app.listen(3000,() => {
     console.log("Server Started");
