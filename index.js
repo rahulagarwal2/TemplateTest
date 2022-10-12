829a3baff5907e86a07ccf4c425098806ae1b67e
Rahul Agarwal <rahula@ZC-RahulAs-MacBook-Pro.local>
2022-10-12 12:01:08 +0530

    About page added

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello world");
-})
+});
+
+app.get('/About', (req, res) => {
+    res.send("Welcome to about page!");
+});
 
 app.listen(3000,() => {
     console.log("Server Started");
